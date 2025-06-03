46e8291cbf9c96471f0210bad13a3b247f59d2cf
Francesco-Zac <francescozaccaro2@gmail.com>
2025-06-03 11:55:13 +0200

    add MainSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Bootstrap from "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import MainSection from "./components/MainSection";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className="App">
+      <MainSection />
+    </div>
+  );
 }
 
 export default App;
