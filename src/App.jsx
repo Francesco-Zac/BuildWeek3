4b60461f190062ccd6c6b3b8a4159eddf110e63d
Borislav Kozhuharski <nomercyforyouss@gmail.com>
2025-06-03 12:30:34 +0200

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import MainSection from "./components/MainSection";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <MainSection />
     </div>
   );
