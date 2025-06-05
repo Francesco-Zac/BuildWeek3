fd30fecac4cb52e1588831ddd19793d1fa33c626
Francesco-Zac <francescozaccaro2@gmail.com>
2025-06-05 15:32:23 +0200

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <Navbar />
       <div className="App">
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/profile" element={<MainSection />} />
         </Routes>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className=" d-flex align-items-center w-100 justify-content-evenly">
         <div className="nav d-flex  gap-1 px-4">
-          <a href="/home">
+          <a href="/">
             <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn" width="41" />
           </a>
           <div className="search-wrapper d-flex align-items-center">
@@ -20,7 +20,7 @@ const Navbar = () => {
 
         <div className="d-flex align-items-center gap-4 ">
           <div className="text-center">
-            <a href="/home">
+            <a href="/">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
                 <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7z" />
               </svg>
